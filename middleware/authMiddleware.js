4b353c21b54c879fac4417a4ae731774d22871d3
aganepaul <paulagane94@gmail.com>
2024-11-28 06:53:26 +0100

    fix server.js, authMiddleware.js and script.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// const authMiddleware = (req, res, next) => {
-//     const token = req.header('Authorization');
-//     if (!token) {
-//         return res.status(401).json({ message: 'Access denied. No token provided.' });
-//     }
-
-//     try {
-//         const verified = jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = verified;
-//         next();
-//     } catch (err) {
-//         res.status(400).json({ message: 'Invalid token.' });
-//     }
-// };
-
 // module.exports = authMiddleware;
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
